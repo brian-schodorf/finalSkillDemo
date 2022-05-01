bfbcbb09137138259c93f08d58050d9c6c88cff9
brian-schodorf <92064101+brian-schodorf@users.noreply.github.com>
2022-05-01 16:26:08 -0700

    update

--- demo.java
+++ demo.java
@@ -1,4 +1,7 @@
 public class demo {
 
-
+    public static int division(int x, int y) {
+        return x/y;
+    }
+    
 }
---
+++ demoTest.java
@@ -0,0 +1,11 @@
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class demoTest {
+
+    @Test
+    public void division() {
+        assertEquals(5, demo.division(5, 8));
+    }
+}
